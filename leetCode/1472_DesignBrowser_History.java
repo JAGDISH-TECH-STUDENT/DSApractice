44b2da09dddc4671391796c814d88c48fe6fc543
JAGDISH <js8724285@gmail.com>
2026-04-27 21:54:10 +0530

    [Jagdish] add 1472_DesignBrowser_History question

---
+++ leetCode/1472_DesignBrowser_History.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+class BrowserHistory {
+    private List<String> history;
+    private int current;
+
+    public BrowserHistory(String homepage) {
+        history = new ArrayList<>();
+        history.add(homepage);
+        current = 0;
+    }
+
+    public void visit(String url) {
+        while (history.size() > current + 1) {
+            history.remove(history.size() - 1);
+        }
+        history.add(url);
+        current++;
+    }
+
+    public String back(int steps) {
+        current = Math.max(0, current - steps);
+        return history.get(current);
+    }
+
+    public String forward(int steps) {
+        current = Math.min(history.size() - 1, current + steps);
+        return history.get(current);
+    }
+    public static void main(String[] args) {
+        BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+        browserHistory.visit("google.com");
+        browserHistory.visit("facebook.com");
+        browserHistory.visit("youtube.com");
+        System.out.println(browserHistory.back(1)); // facebook.com
+        System.out.println(browserHistory.back(1)); // google.com
+        System.out.println(browserHistory.forward(1)); // facebook.com
+        browserHistory.visit("linkedin.com");
+        System.out.println(browserHistory.forward(2)); // linkedin.com
+        System.out.println(browserHistory.back(2)); // google.com
+        System.out.println(browserHistory.back(7)); // leetcode.com
+    }
+}
